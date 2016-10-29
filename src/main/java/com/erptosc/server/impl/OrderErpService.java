05ffb369c7c7834f33b4bc7ad2772bb98aa21384
zhanrui208 <271912564@qq.com>
2016-10-29 17:52:49 +0800

    10291

--- src/main/java/com/erptosc/server/impl/OrderErpService.java
+++ src/main/java/com/erptosc/server/impl/OrderErpService.java
@@ -3,17 +3,24 @@ package com.erptosc.server.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.common.httpClient.HttpHelper;
 import com.common.until.PropertiesUtils;
+import com.erptosc.control.orderControl;
 import com.erptosc.server.iOrderErpService;
 
 @Service
 public class OrderErpService implements iOrderErpService{
 
+//	@Autowired
+//	orderControl order;
+	
 	@Override
 	public String getOrder() {
+//		order.getOrder(null);
+		
 		Map<String,String> params = new HashMap<String,String>();
 		params.put("id", "1001");
 		params.put("name", "ERP传数据到商城！");
